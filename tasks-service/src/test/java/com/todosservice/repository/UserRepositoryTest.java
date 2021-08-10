946c7f4a2771d46416a980c51d9c7099697691d1
Roger-AVP <rogervaca1995@gmail.com>
2021-08-10 17:46:26 -0500

    update

--- tasks-service/src/test/java/com/todosservice/repository/UserRepositoryTest.java
+++ tasks-service/src/test/java/com/todosservice/repository/UserRepositoryTest.java
@@ -1,25 +1,42 @@
 package com.todosservice.repository;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.todosservice.entity.User;
+import com.todosservice.specification.UserSpecification;
 
 @SpringBootTest
+@Transactional
 public class UserRepositoryTest {
 
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Test
 	public void testSaveUser() {
-		User user = new User(null, "rogers", "rogers@gmail.com", "12345", null);
-		User userTodo = userRepository.save(user);
+		User user = new User();
+		user.setName("rogerv");
+		user.setEmail("rogerv@gmail.com");
+		user.setPassword(passwordEncoder.encode("12345"));
 
+		User userTodo = userRepository.save(user);
 		assertNotNull(userTodo);
+	}
+
+	@Test
+	void SpecificationFindByEmail() {
+		User user = userRepository.findOne(UserSpecification.findByEmail("roger@gmail.com")).orElse(null);
+		assertNotNull(user);
 	}
 
 }
